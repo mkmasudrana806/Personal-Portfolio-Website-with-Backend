f6865ff3091a26ca08f47986a77de1a82b1c6b61
mkmasudrana806 <mkmasudrana806@gmail.com>
2026-02-07 23:44:45 +0600

    refactor: Remove baner page

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,12 @@ import ProjectsBackend from "@/components/projectsBackend";
 import EngineeringDecisionBackend from "@/components/backendDecisionAndPractices";
 import AboutMeBackend from "@/components/aboutMeBackend";
 import FloatingProfileBackend from "@/components/floatingProfileBackend";
-import { LinkedInBanner } from "@/components/banner";
 
 export default function Home() {
   return (
     <div className="w-full">
       <HeaderBackend />
       <HeroBackend />
-      <LinkedInBanner />
       <SkillsBackend />
       <ProjectsBackend />
       <EngineeringDecisionBackend />
